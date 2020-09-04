4b28e74e8f51b1075cecf2c2a383acdbc0e84222
Adrian Hernandez <adrianhdez929@gmail.com>
2020-09-04 13:30:29 -0400

    renamed options.config to options.conffile

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ class Options(object):
         self.toaddress = None
         self.new = False
         self.datadir = determine_db_dir()
-        self.config = "crown.conf"
+        self.conffile = "crown.conf"
         self.fee = "0.025"
         self.amount = None
         self.upto = None
